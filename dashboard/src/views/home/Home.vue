26fd8b5441785a8d9ba5f7f71767f4311e803709
Robson de Moraes Silva <rms2307@outlook.com>
2023-01-27 23:41:55 -0300

    Iniciando implementação da view home

--- dashboard/src/views/home/Home.vue
+++ dashboard/src/views/home/Home.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1 class="text-brand-main">Home</h1>
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedback @ 2023</p>
+  </div>
 </template>
+
+<script>
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+import { useRouter } from 'vue-router'
+import { onMounted } from '@vue/runtime-core'
+
+export default {
+  components: { CustomHeader, Contact },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedback' })
+      }
+    })
+
+    function handleLogin () {}
+
+    function handleAccountCreate () {}
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
+}
+</script>
