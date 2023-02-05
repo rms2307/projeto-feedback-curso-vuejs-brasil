ce9a297ac408f998d26be92c4c644b1f8b4020f6
Robson de Moraes Silva <rms2307@outlook.com>
2023-02-05 19:40:04 -0300

    Implementando criação de conta

--- dashboard/src/services/authService.js
+++ dashboard/src/services/authService.js
@@ -1,6 +1,30 @@
 export default (httpClient) => ({
+  register: async ({ name, email, password }) => {
+    const response = await httpClient.post("/auth/register", {
+      name,
+      email,
+      password,
+    });
+
+    let errors = null;
+
+    if (!response.data) {
+      errors = {
+        status: response.request.status,
+        statusText: response.request.status,
+      };
+    }
+
+    return {
+      data: response.data,
+      errors,
+    };
+  },
   login: async ({ email, password }) => {
-    const response = await httpClient.post("/auth/login", { email, password });
+    const response = await httpClient.post("/auth/login", {
+      email,
+      password,
+    });
 
     let errors = null;
 
