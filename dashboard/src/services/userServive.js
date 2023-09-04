3d3de3d4da9ca57826067d870bf2f48d0167c07c
Robson de Moraes Silva <rms2307@outlook.com>
2023-09-03 22:08:22 -0300

    Corrigindo index de services

--- dashboard/src/services/userServive.js
+++ dashboard/src/services/userServive.js
@@ -1,7 +1,6 @@
 export default (httpClient) => ({
   getMe: async () => {
     const response = await httpClient.get('/users/me')
-    console.log('httpClient', httpClient.headers)
     return {
       data: response.data
     }
